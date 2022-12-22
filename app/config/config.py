30d279d8cb4b3be92d93a2e95190c035b54e5453
Ivan Bogomolov <qhomop@gmail.com>
2022-12-22 19:47:15 +0300

    main

--- app/config/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://root:root@172.18.0.3:5432/skat")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
