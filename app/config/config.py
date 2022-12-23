1793e20bc92cb827b9b4c207938caef39feef698
Ivan Bogomolov <qhomop@gmail.com>
2022-12-23 03:33:46 +0300

    cors

--- app/config/config.py
+++ app/config/config.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://root:root@172.27.0.2:5432/dpp_dev")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
